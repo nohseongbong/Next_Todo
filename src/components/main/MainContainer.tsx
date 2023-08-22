2f6f984b3035c9e7a14993004d2aac48390810dd
nohseongbong <shtjdqhd@naver.com>
2023-08-22 20:43:28 +0900

    feat: 로그인 및 생성 페이지 이동 버튼 추가

--- src/components/main/MainContainer.tsx
+++ src/components/main/MainContainer.tsx
@@ -1,14 +1,16 @@
 import { useEffect, useState } from "react";
 import styles from "@/styles/Main.module.css";
-import { Container } from "react-bootstrap";
+import { Button, Container } from "react-bootstrap";
 import { getTodos } from "@/apis/todo/todo";
 import { useQuery } from "@tanstack/react-query";
 import TodoList from "./TodoList";
 import TodoSearch from "./TodoSearch";
 import TodoPagination from "./TodoPagination";
 import useTodoStore from "@/store/todoStore";
+import { useRouter } from "next/router";
 
 const MainContainer = () => {
+  const router = useRouter();
   const { data } = useQuery({ queryKey: ["todos"], queryFn: getTodos, refetchOnWindowFocus: false });
   const { actions, todos } = useTodoStore((store) => store);
 
@@ -17,7 +19,7 @@ const MainContainer = () => {
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const keyword = event.target.value;
     if (keyword === "") {
-      actions.setTodos(data?.todos ?? []);
+      actions.setTodos(data ?? []);
     } else {
       const filteredTodos = todos.filter((todo) => todo.title.toLowerCase().includes(keyword.toLowerCase()));
       actions.setTodos(filteredTodos);
@@ -25,13 +27,21 @@ const MainContainer = () => {
   };
 
   useEffect(() => {
-    actions.setTodos(data?.todos ?? []);
+    actions.setTodos(data ?? []);
   }, [actions, data]);
 
   return (
     <Container className={styles.container}>
       <TodoSearch handleSearchChange={handleSearchChange} />
       <TodoList currentPage={currentPage} todos={todos} />
+      <div style={{ display: "flex", gap: 10, margin: "10px 0px" }}>
+        <Button onClick={() => router.push("/login")} variant="secondary" className="ml-2">
+          Login
+        </Button>
+        <Button onClick={() => router.push("/create")} variant="secondary" className="ml-2">
+          Create
+        </Button>
+      </div>
       <TodoPagination current={{ currentPage, setCurrentPage }} todos={todos} />
     </Container>
   );
