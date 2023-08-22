f5fccb9c3c3d478f76499cb7f45378743ba92bcc
nohseongbong <shtjdqhd@naver.com>
2023-08-22 20:40:31 +0900

    feat: JWT 토큰 만료시 재발급

--- src/apis/config.ts
+++ src/apis/config.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { renewToken } from "./auth/auth";
 export const apiUrl = process.env.NEXT_PUBLIC_API_URL ?? "";
 
 export const axiosInstance = axios.create({
@@ -8,11 +9,14 @@ export const axiosInstance = axios.create({
 
 axiosInstance.interceptors.request.use(
   async (config) => {
+    const accessToken = localStorage.getItem("jwtAccessKey");
     config.headers = config.headers ?? {};
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
     return config;
   },
   (error) => {
-    console.log("Request Api Error");
     return Promise.reject(error);
   }
 );
@@ -22,7 +26,23 @@ axiosInstance.interceptors.response.use(
     return response;
   },
   async (error) => {
-    console.log("Response Api Error");
+    if (error.response?.status === 401) {
+      const config = error.config;
+      const accessToken = localStorage.getItem("jwtAccessKey");
+      const refreshToken = localStorage.getItem("jwtRefreshKey");
+      if (refreshToken && accessToken && config) {
+        console.log("accessToken 재발급 요청");
+        try {
+          const data = await renewToken({ accessToken, refreshToken });
+          if (data?.accessToken) {
+            console.log(data?.accessToken, "accessToken 재발급 성공");
+            localStorage.setItem("jwtAccessKey", data.accessToken);
+          }
+        } catch {
+          console.log("accessToken 재발급 실패");
+        }
+      }
+    }
     return Promise.reject(error);
   }
 );
