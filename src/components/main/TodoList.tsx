89fa660c587d5766a6a7497828690417d40fde29
nohseongbong <shtjdqhd@naver.com>
2023-08-22 20:43:37 +0900

    feat: 삭제 버튼 추가

--- src/components/main/TodoList.tsx
+++ src/components/main/TodoList.tsx
@@ -1,8 +1,10 @@
-import { Todo } from "@/pages/api/todoList";
-import { Container, Accordion } from "react-bootstrap";
+import { deleteTodo, updateTodo } from "@/apis/todo/todo";
+import { TodoType } from "@/types/todo";
+import { useMutation } from "@tanstack/react-query";
+import { Container, Accordion, Button } from "react-bootstrap";
 
 interface TodoListProps {
-  todos: Todo[];
+  todos: TodoType[];
   currentPage: number;
 }
 
@@ -13,14 +15,29 @@ const TodoList = ({ todos, currentPage }: TodoListProps) => {
   const endIndex = startIndex + ITEMS_PER_PAGE;
 
   const itemsToShow = todos.slice(startIndex, endIndex);
+
+  const mutation = useMutation(deleteTodo, {
+    onSuccess: (data) => {
+      console.log(data, ": data");
+    },
+  });
+  const onClickDelete = (id: number) => {
+    mutation.mutate({ id });
+  };
+
   return (
     <Container>
       <Accordion defaultActiveKey="0">
         {itemsToShow.map((todo) => {
           return (
-            <Accordion.Item key={todo.todoId} eventKey={todo.todoId}>
+            <Accordion.Item key={todo.id} eventKey={String(todo.id)}>
               <Accordion.Header>{todo.title}</Accordion.Header>
-              <Accordion.Body>{todo.content}</Accordion.Body>
+              <Accordion.Body>
+                {todo.content}
+                <Button onClick={() => onClickDelete(todo.id)} variant="secondary" className="ml-2">
+                  삭제
+                </Button>
+              </Accordion.Body>
             </Accordion.Item>
           );
         })}
