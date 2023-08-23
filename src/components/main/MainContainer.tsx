4e072803a8a616aca6094035283a60b3f3e86d6b
nohseongbong <shtjdqhd@naver.com>
2023-08-23 16:53:24 +0900

    refactor: header 컴포넌트 분리

--- src/components/main/MainContainer.tsx
+++ src/components/main/MainContainer.tsx
@@ -1,17 +1,16 @@
 import { useEffect, useState } from "react";
 import styles from "@/styles/Main.module.css";
-import { Button, Container } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import { getTodos } from "@/apis/todo/todo";
 import { useQuery } from "@tanstack/react-query";
 import TodoList from "./TodoList";
 import TodoSearch from "./TodoSearch";
 import TodoPagination from "./TodoPagination";
 import useTodoStore from "@/store/todoStore";
-import { useRouter } from "next/router";
+import { QUERY_KEYS } from "@/constants/query-key";
 
 const MainContainer = () => {
-  const router = useRouter();
-  const { data } = useQuery({ queryKey: ["todos"], queryFn: getTodos, refetchOnWindowFocus: false });
+  const { data } = useQuery({ queryKey: [QUERY_KEYS.TODOS], queryFn: getTodos, refetchOnWindowFocus: false });
   const { actions, todos } = useTodoStore((store) => store);
 
   const [currentPage, setCurrentPage] = useState<number>(1);
@@ -34,14 +33,6 @@ const MainContainer = () => {
     <Container className={styles.container}>
       <TodoSearch handleSearchChange={handleSearchChange} />
       <TodoList currentPage={currentPage} todos={todos} />
-      <div style={{ display: "flex", gap: 10, margin: "10px 0px" }}>
-        <Button onClick={() => router.push("/login")} variant="secondary" className="ml-2">
-          Login
-        </Button>
-        <Button onClick={() => router.push("/create")} variant="secondary" className="ml-2">
-          Create
-        </Button>
-      </div>
       <TodoPagination current={{ currentPage, setCurrentPage }} todos={todos} />
     </Container>
   );
