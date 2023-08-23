e5ac129cb38882b460b67dafb6f064a79bc4453e
nohseongbong <shtjdqhd@naver.com>
2023-08-23 16:54:22 +0900

    feat: header 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,17 @@ import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "@/components/layout/header";
 
 export default function App({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
+      <Header />
+      <div className="layout_component">
+        <Component {...pageProps} />
+      </div>
       <ReactQueryDevtools initialIsOpen={process.env.NODE_ENV === "development"} />
     </QueryClientProvider>
   );
