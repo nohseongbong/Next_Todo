f7a4959fce76845b12cab34ab71e9f482f1d4890
nohseongbong <shtjdqhd@naver.com>
2023-08-25 18:12:45 +0900

    refactor: 불필요한 store 값 제거 (TODO:: 수정 예정)

--- src/store/todoStore.ts
+++ src/store/todoStore.ts
@@ -1,38 +1,9 @@
-import { TodoType } from "@/types/todo";
 import { createZustandStore } from "./middleware";
 
-type TodoStore = {
-  todos: TodoType[];
-  filteredTodos: TodoType[];
-};
+type TodoStore = {};
 
-type StoreActions = {
-  actions: {
-    setTodos: (todos: TodoType[]) => void;
-    setSearchKeyword: (keyword: React.ChangeEvent<HTMLInputElement>) => void;
-  };
-};
+type StoreActions = {};
 
-const useTodoStore = createZustandStore<TodoStore & StoreActions>((set) => ({
-  todos: [],
-  filteredTodos: [],
-  actions: {
-    setTodos(todos) {
-      set({ todos: todos, filteredTodos: todos });
-    },
-    setSearchKeyword(event: React.ChangeEvent<HTMLInputElement>) {
-      const keyword = event.target.value;
-      if (keyword === "") {
-        set((state) => ({ filteredTodos: state.todos }));
-      } else {
-        set((state) => ({
-          filteredTodos: state.todos.filter(
-            (todo) => todo.title.toLowerCase().includes(keyword.toLowerCase()) || todo.content.toLowerCase().includes(keyword.toLowerCase())
-          ),
-        }));
-      }
-    },
-  },
-}));
+const useTodoStore = createZustandStore<TodoStore & StoreActions>((set) => ({}));
 
 export default useTodoStore;
